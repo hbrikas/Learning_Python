0a5b4d366fa7c91008b5ab01f6bd8e03ca004371
Hari Brikas <73021397+hbrikas@users.noreply.github.com>
2020-11-02 20:03:22 +0200

    Update learn_if.py

--- learn_if.py
+++ learn_if.py
@@ -1,4 +1,5 @@
 import turtle
+import os
 
 # Setup Window
 window = turtle.Screen()
@@ -9,12 +10,62 @@ window.title("If")
 # Setup Turtle
 bill = turtle.Turtle()
 bill.color ("red")
+bill.penup()
 
 #define moving step
 step = 15
 
 #define moving functions
-def up():
+def move_left():
+    x=bill.xcor()
+    x -= step
+    if x < -280:
+        x = -280
+    bill.setheading(180)
+    bill.setx(x)
+
+def move_right():
+    x=bill.xcor()
+    x += step
+    if x> 280:
+        x=280
+    bill.setheading(0)
+    bill.setx(x)
+
+def move_up():
+    y=bill.ycor()
+    y += step
+    if y > 280:
+        y=280
+    bill.setheading(90)
+    bill.sety(y)
+
+def move_down():
+    y=bill.ycor()
+    y -= step
+    if y < -280:
+        y=-280
+    bill.setheading(270)
+    bill.sety(y)
+
+
+    
+
+
+
+
+#Keyboard bindings
+turtle.listen()
+turtle.onkey (move_left, "Left")
+turtle.onkey (move_right, "Right")
+turtle.onkey (move_up, "Up")
+turtle.onkey (move_down, "Down")
+
+
+
+
+
+
     
 
 
